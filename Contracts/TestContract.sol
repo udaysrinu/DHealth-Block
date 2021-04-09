f05daf2777982fcfe69baea43d9454be1dc5f8f5
waytoashutosh <45975542+waytoashutosh@users.noreply.github.com>
2021-04-10 03:11:27 +0530

    Update TestContract.sol

--- Contracts/TestContract.sol
+++ Contracts/TestContract.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.3;
 
 
@@ -13,6 +14,7 @@ contract D_Health_Block{
         
     }
     struct patientData{
+        
         string name_of_doc;
         string hospital;
         uint256 license;
@@ -33,12 +35,31 @@ contract D_Health_Block{
     mapping(uint256 => doctor) public dMap;
     
     
-    mapping(uint256 => patient[]) public pList;
-    mapping(uint256 => doctor[]) public dList;
+    mapping(uint256 => patient[]) public pDataList;
+    mapping(uint256 => doctor[]) public dDataList;
+    
+    // patientData newData;
+    // function addPatientData(uint256 _id, string memory _doc_name, string memory _hospital, uint256 _license, uint256 _date, string memory _disease) public {
+        
+    //     pDataList[_id].push(patientData(
+    //         {
+    //             name_of_doc:_doc_name,
+    //             hospital:_hospital,
+    //             license:_license,
+    //             date:_date,
+    //             disease:_disease
+                
+    //         })
+    //     );
+    // }
+    
+    // function getSinglePatientData(uint256 _id) public{
+        
+    // }
     
     patient newPatient;
     function createPatient(string memory _name,uint256 _adhar,address _account_address) public {
-        
+      
         newPatient = patient(pID,_name,_adhar,_account_address);
         pMap[pID]=newPatient;
         pID++;
@@ -47,7 +68,7 @@ contract D_Health_Block{
     
     function updatePatient(uint256 _id,string memory _name,uint256 _adhar,address _account_address) public {
         
-        require(_id<pID,"Patient with given ID doesn't exist");
+        require(_id>pID,"Patient with given ID doesn't exist");
         
         pMap[_id].name=_name;
         pMap[_id].adhar=_adhar;
@@ -65,9 +86,12 @@ contract D_Health_Block{
     
     function updateDoctor(uint256 _license,string memory _name,string memory _hospital) public {
         
-        require(licenseExists[_license]==true, "You have already registered");
+        
         dMap[_license].name = _name;
         dMap[_license].hospital = _hospital;
     }
+    
+    
+    
 
 }
