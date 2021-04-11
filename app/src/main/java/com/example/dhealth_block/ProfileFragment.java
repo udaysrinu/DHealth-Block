afa006fd78d9ed87b7265cc542f2f96f8cd87f70
ashwiniswag <ashwinipal360@gmail.com>
2021-04-11 06:00:50 +0530

    Profile fragment

--- app/src/main/java/com/example/dhealth_block/ProfileFragment.java
+++ app/src/main/java/com/example/dhealth_block/ProfileFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
+
+    TextView fullname,id,aadharno;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -16,6 +19,10 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v=inflater.inflate(R.layout.fragment_profile, container, false);
+        fullname=v.findViewById(R.id.fulnametxt);
+        id=v.findViewById(R.id.idtxt);
+        aadharno=v.findViewById(R.id.aadharnotxt);
+        return v;
     }
 }
